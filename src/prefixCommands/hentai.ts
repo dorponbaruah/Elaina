7c1bda509c2822adb90dfb0fab8229570865c616
dorponjeetbaruah101@gmail.com <dorponjeetbaruah101@gmail.com>
2022-09-13 00:09:00 +0530

    add: hentai command 😬

---
+++ src/prefixCommands/hentai.ts
@@ -0,0 +1,42 @@
+import { ElainaPrefixCommand, ElainaWebhook, RedditFetch, constants } from "../index";
+import { MessageEmbed } from "discord.js";
+
+export default new ElainaPrefixCommand({
+  name: "hentai",
+  description: "Lewd anime/manga images UwU.",
+  aliases: ["h"],
+  category: "Forest of witches",
+  onlyChannels: "hentai-region",
+  run: (client, message, args) => {
+    const subreddits = [
+        "hentai",
+        "dekaihentai",
+        "ecchi",
+        "hentaimemes",
+        "hentaiBreeding",
+        "hentai_gif",
+        "westernHentai",
+        "yiff",
+        "waifusgonewild",
+        "hentaibondage",
+        "deliciousTraps",
+        "thick_hentai"
+    ];
+    
+    const post = new RedditFetch(subreddits);
+    
+    await post.makeRequest();
+    
+    new ElainaWebhook({ channelId: message.channel.id }, {
+      username: post.getSubredditName,
+      avatarURL: post.getSubredditIcon,
+      embeds: [
+        new MessageEmbed()
+          .setDescription(post.getPostTitle)
+          .setImage(post.getPostImage)
+          .setColor(constants.Colors.MAIN_EMBED_COLOR)
+          .setFooter({ text: `Sent by ${client.user?.tag}`, iconURL: client.user?.displayAvatarURL() })
+      ]
+    });
+  }
+});
